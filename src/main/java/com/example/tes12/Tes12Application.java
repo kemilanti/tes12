ff7490a252f718123e3475d06a8064bf1b376336
kemilanti <91670603+kemilanti@users.noreply.github.com>
2022-09-15 18:16:45 +0100

    Initial commit

---
+++ src/main/java/com/example/tes12/Tes12Application.java
@@ -0,0 +1,13 @@
+package com.example.tes12;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tes12Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Tes12Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/tes12/Tes12ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tes12;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tes12ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
